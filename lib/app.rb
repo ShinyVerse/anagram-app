622077e6f635ff9e1c532c6d88ea3c1b102248a4
Laura Jackson <playeronereadystart@gmail.com>
2019-01-22 21:49:36 +0000

    add start_app method and test

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,31 @@
+require_relative 'anagram'
+
 class App
+  def initialize(anagram = Anagram.new)
+    @anagram = anagram
+  end
+
   def parse_input(list)
-    fail if validate(list)
-    p "the list passed parse_input: #{list}"
+    raise ArgumentError if validate(list)
     if list.end_with?(".txt")
-      @user_list = File.open(list).read.split("\n")
+      file = File.expand_path('../list.txt', File.dirname(__FILE__))
+      @user_list = File.open(file).read.split("\n")
     else
       @user_list = list.split(" ")
+    end
+  end
+
+  def start_app
+    puts 'Enter a word and receive anagrams from the list of your word'
+    puts 'Write "exit" to finish'
+    word = ''
+    @anagram.update_list(@user_list)
+    while word != 'exit'
+      puts 'Please enter your word to check'
+      word = $stdin.gets.chomp
+      if word != 'exit'
+        puts "The anagrams found are : #{@anagram.find_anagrams(word)}"
+      end
     end
   end
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,21 +1,32 @@
 require 'app'
 
 describe App do
-  let(:app) { described_class.new() }
+  let(:anagram) { double('Anagram double') }
+  let(:app) { described_class.new(anagram) }
 
   context 'parse_input' do
     it 'can take a file as an argument' do
-
       input_list = app.parse_input(File.dirname(__FILE__) + '/list.txt')
-      expect(input_list).to eq ["era","cat","rat","are","pear","ear"]
+      expect(input_list).to eq %w[era cat rat are pear ear]
     end
 
     it 'can take input of string as list' do
-      expect(app.parse_input("era cat rat are pear ear")).to eq ["era","cat","rat","are","pear","ear"]
+      expect(app.parse_input('era cat rat are pear ear')).to eq %w[era cat rat are pear ear]
     end
 
     it 'errors if passed arg that is not a string' do
-      expect{app.parse_input(4)}.to raise_error
+      expect { app.parse_input(4) }.to raise_error
+    end
+  end
+
+  context 'start_app' do
+    it 'calls find_anagrams when word is entered' do
+      allow(anagram).to receive(:update_list)
+      allow(anagram).to receive(:find_anagrams)
+      allow(STDIN).to receive(:gets).and_return("tac\n", "exit\n")
+
+      app.start_app
+      expect(anagram).to have_received(:find_anagrams)
     end
   end
 end
