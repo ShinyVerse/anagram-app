699ab05636221cbd237989515c02cbe56736a1b9
Laura Jackson <playeronereadystart@gmail.com>
2019-01-22 19:11:45 +0000

    add validation to parse_input

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,17 @@
 class App
-  def get_list(list = Kernel.gets.chomp)
+  def parse_input(list)
+    fail if validate(list)
+    p "the list passed parse_input: #{list}"
     if list.end_with?(".txt")
-      @user_list = File.open(list).read().split("\n")
+      @user_list = File.open(list).read.split("\n")
     else
       @user_list = list.split(" ")
     end
-    p @user_list
   end
 
+  private
+
+  def validate list
+    !list.is_a?(String)
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,13 +3,19 @@ require 'app'
 describe App do
   let(:app) { described_class.new() }
 
-  context 'get_list' do
+  context 'parse_input' do
     it 'can take a file as an argument' do
-      expect(app.get_list(File.dirname(__FILE__) + '/list.txt')).to eq ["era","cat","rat","are","pear","ear"]
+
+      input_list = app.parse_input(File.dirname(__FILE__) + '/list.txt')
+      expect(input_list).to eq ["era","cat","rat","are","pear","ear"]
     end
 
     it 'can take input of string as list' do
-      expect(app.get_list("era cat rat are pear ear")).to eq ["era","cat","rat","are","pear","ear"]
+      expect(app.parse_input("era cat rat are pear ear")).to eq ["era","cat","rat","are","pear","ear"]
+    end
+
+    it 'errors if passed arg that is not a string' do
+      expect{app.parse_input(4)}.to raise_error
     end
   end
 end
