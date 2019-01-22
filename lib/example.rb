a4ff0009f8642c40b579ef6f8f639d47a659642a
Laura Jackson <playeronereadystart@gmail.com>
2019-01-22 14:55:41 +0000

    remove setup tester files

--- lib/example.rb
+++
@@ -1,5 +0,0 @@
-class Example
-  def tester_function
-     2 + 2
-  end
-end
--- spec/example_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'example'
-
-describe Example do
-  it 'returns 42' do
-    expect(subject.tester_function).to eq 4
-  end
-end
